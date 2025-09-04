6a96fde1953c3330640c8571f92afda54033f038
Dogeonmin <dgm000624@gmail.com>
2025-09-04 16:16:41 +0900

    Add Java files

---
+++ IdeaProjects/StringClassTest00/src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(54);
+        sb.append("java");
+        sb.append("ok");
+        sb.append(true);
+        sb.append(10.04);
+        System.out.println(sb);
+
+    }
+}
